e1c9b3923d6da873e2af07c451d86a507c2433e7
Jared Staples <jstaplesmusic@gmail.com>
2023-01-10 17:18:21 -0500

    Initial commit with folder structure setup

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// EXAMPLE MODEL INDEX FILE
+
+// const User = require('./User');
+// const Post = require('./Post');
+
+// module.exports = { User, Post };
---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+// EXAMPLE API INDEX FILE
+
+// const router = require('express').Router();
+// const postRoutes = require('./postRoutes');
+// const userRoutes = require('./userRoutes');
+
+// router.use('/posts', postRoutes);
+// router.use('/users', userRoutes);
+
+// module.exports = router;
